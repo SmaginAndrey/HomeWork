b8531de4d009d1001e1cf10afc9295ea427c3ba6
Andrey Smagin <an.sm4gin@yandex.ru>
2023-11-29 16:43:43 +0400

    Сделал правильный вывод окончаний слов

--- Task2_2.java
+++ Task2_2.java
@@ -9,10 +9,26 @@ public class Task2_2 {
             long day = minutes/1440;
             long hour = (minutes - 1440*(minutes/1440))/60;
             long minute = ((minutes - 1440 * day) - hour*60);
-            System.out.println(day + " день" + " " + hour + " часов" + " " + minute + " минут");
+            System.out.println("Это: ");
+            Format.outputOptions(day,"день", "дня", "дней");
+            Format.outputOptions(hour,"час", "часа", "часов");
+            Format.outputOptions(minute,"минута", "минуты", "минут");
         }
 
     }
+    class Format {
+        static void outputOptions(long time, String oneFormat, String twoFormat, String threeFormat) {
+            long t = time % 10;
+            if (t == 1)
+                System.out.println(time + " " + oneFormat);
+            else if (t == 2 || t == 3 || t == 4)
+                System.out.println(time + " " + twoFormat);
+            t = time % 100;
+            if (5 <= t && t <= 20)
+                System.out.println(time + " " + threeFormat);
+        }
+    }
+
     private static long getMinutes(String[] args) {
         if (args.length == 1) {
             return Long.parseLong(args[0]);
